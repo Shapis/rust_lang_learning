fa75d1ecf5dde6b18c553446498bdd8a59e96f0e
Henrique da Silva <hpsilva@pm.me>
2024-04-03 13:16:02 -0700

    Add chapter 7 rust lang learning

--- The Rust Book/Chapter 6/enums/src/main.rs
+++ The Rust Book/Chapter 6/enums/src/main.rs
@@ -1,3 +1,12 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
 fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
     println!("Hello, world!");
 }
